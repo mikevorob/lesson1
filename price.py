99d08d09011adbab9dead5529c9f6e80036261d6
Mikhail Vorobyev <mikevorob@gmail.com>
2018-09-08 18:16:05 +0300

    Сделал бота-попугая)))

--- price.py
+++ price.py
@@ -1,5 +1,7 @@
 def format_price(price):
-	res_price=int(price)
-	return ('Цена: '+ str(res_price) + ' руб.')
+    res_price=int(price)
+    return ('Цена: '+ str(res_price) + ' руб.')
+
+    
 display_price=format_price(56.24)
 print(display_price)
